5555e655a2f137cad131eb19c1d3b584564d39b4
Raj <rajkar921@gmail.com>
2021-04-22 03:41:15 +0530

    Edit Index Page - added features on features area. - added course category. -  youtube videos. -  features. - developers identity. - fix navbar in contact page. - fix background color in home page.

--- home/views.py
+++ home/views.py
@@ -1,12 +1,22 @@
+import requests
+
 from django.shortcuts import render, HttpResponse
+from requests.api import get
 from .models import Contact
 from .send_email import sendEmail
 
 # Create your views here.
+def get_data(endpoint):
+    response = requests.get(endpoint)
+    return response.json()
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', {
+        'courses': get_data('https://api.npoint.io/53897478e13604a83e77'),
+        'yt_videos': get_data('https://api.npoint.io/1e3ad07b9c9b7fcf4f20'),
+        'developers': get_data('https://api.npoint.io/952ccba723fbe7772407')
+    })
 
 
 def about(request):
