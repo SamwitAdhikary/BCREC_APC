61c34483fe12ab630afc1ae996b97a8c2223f30c
Raj <rajkar921@gmail.com>
2021-04-23 16:50:57 +0530

    added verify-otp rouote

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="HomePage"),
     path('about/', views.about, name="AboutPage"),
+    path('verify-otp/', views.verify_user, name="verifyUser"),
     # path('courses/', views.courses, name="CoursesPage"),
     # path('courses-details/', views.courses_details, name="CoursesDetailsPage"),
     path('contact/', views.contact, name="ContactPage"),
