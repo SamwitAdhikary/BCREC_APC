c9c14ce36ef7e3238349c494e8a80894ffe307e5
Raj <rajkar921@gmail.com>
2021-04-27 21:30:43 +0530

    Reset Password feature added. - now user can reset password.

--- bcrecapc/settings.py
+++ bcrecapc/settings.py
@@ -153,3 +153,11 @@ MESSAGE_TAGS = {
     messages.INFO: '',
     50: 'critical',
 }
+
+# SMTP Configuratiion
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'yourEmailPassword'
+EMAIL_HOST_PASSWORD = 'yourEmailPassword'
