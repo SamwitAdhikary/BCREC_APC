b5f3665ee5c477a7f84c0c4d4fb5248304f64ba7
Raj-kar <rajkar921@gmail.com>
2021-05-08 18:54:02 +0530

    Add minor changes - add apc logo and favicon. - add course and semester selection field. - remove unnecessary photos from static.

--- home/forms.py
+++ home/forms.py
@@ -8,18 +8,21 @@ class CreateUserFrom(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CreateUserFrom, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.pop('autofocus')
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
 
 class UpdateForm(forms.ModelForm):
     class Meta:
         model = UserProfileInfo
         fields = [
-            'name', 'phone_number', 'college_name', 'state', 'bio', 'portfolio_site', 'upload_cv'
+            'name', 'phone_number', 'college_name', 'state', 'bio', 'portfolio_site', 'upload_cv', 'course', 'Semester'
         ]
 
         # fields = '__all__'
+
 
 class UpdateImageForm(forms.ModelForm):
     class Meta:
